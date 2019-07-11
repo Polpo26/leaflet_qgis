d0831c19d67c05d82f55db6a14d7aa705d198743
Polpo26 <49679598+Polpo26@users.noreply.github.com>
2019-07-11 10:38:47 +0200

    Add files via upload

---
+++ leaflet/data/mastermaptopo_3035070_0CartographicSymbol_4.js
@@ -0,0 +1,9 @@
+var json_mastermaptopo_3035070_0CartographicSymbol_4 = {
+"type": "FeatureCollection",
+"name": "mastermaptopo_3035070_0CartographicSymbol_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "TOID": "1000001899050496", "FeatureCode": 10066.0, "Version": 3.0, "VersionDate": "2008-07-15T00:00:00.000", "Theme": "Terrain And Height", "DescriptiveGroup": "Height Control", "DescriptiveTerm": "Bench Mark", "Orientation": 942.0, "OrientationDegrees": 94.2, "PhysicalLevel": "50" }, "geometry": { "type": "Point", "coordinates": [ -0.074657613752335, 51.551212926541631 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "TOID": "1000001899050502", "FeatureCode": 10066.0, "Version": 3.0, "VersionDate": "2008-07-15T00:00:00.000", "Theme": "Terrain And Height", "DescriptiveGroup": "Height Control", "DescriptiveTerm": "Bench Mark", "Orientation": 2745.0, "OrientationDegrees": 274.5, "PhysicalLevel": "50" }, "geometry": { "type": "Point", "coordinates": [ -0.07282133481712, 51.552085177983045 ] } }
+]
+}
